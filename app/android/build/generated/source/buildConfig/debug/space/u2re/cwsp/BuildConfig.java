6bfae3117133848717e75a3d15476b682c18c0eb
L2NE-dev <conway24b@gmail.com>
2026-08-27 13:06:31 +0700

    older but better icon

--- app/android/build/generated/source/buildConfig/debug/space/u2re/cwsp/BuildConfig.java
+++ app/android/build/generated/source/buildConfig/debug/space/u2re/cwsp/BuildConfig.java
@@ -7,12 +7,12 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "space.u2re.cwsp";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 189;
-  public static final String VERSION_NAME = "0.0.187";
+  public static final int VERSION_CODE = 190;
+  public static final String VERSION_NAME = "0.0.188";
   // Field from default config.
   public static final boolean CWSP_LAUNCHER_SKU = false;
   // Field from default config.
-  public static final int CWSP_VERSION_CODE = 189;
+  public static final int CWSP_VERSION_CODE = 190;
   // Field from default config.
-  public static final String CWSP_VERSION_NAME = "0.0.187";
+  public static final String CWSP_VERSION_NAME = "0.0.188";
 }
